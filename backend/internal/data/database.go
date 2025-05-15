1a123c4d1ff406f685c42711cc9bf87dea0a98eb
Rafi Yudhistira <rafiyudhistira689@gmail.com>
2025-05-15 16:01:17 +0700

    add database connection handling and environment variable loading

--- backend/internal/data/database.go
+++ backend/internal/data/database.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -20,5 +21,26 @@ func initDB() *sql.DB {
 
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error pinging database: ", err)
+		return db
+	}
+	return db
+}
+func closeDB(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		log.Fatal("Error closing database: ", err)
+	}
 }
