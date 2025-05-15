9e51a2a5e5569b1c011c0c1dc79f99a5e0f8b114
Rafi Yudhistira <rafiyudhistira689@gmail.com>
2025-05-15 15:48:00 +0700

    bisa nyambung ke databse browww

---
+++ backend/internal/data/database.go
@@ -0,0 +1,24 @@
+// buat handle koneksi ke database
+
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func initDB() *sql.DB {
+	// load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+}
